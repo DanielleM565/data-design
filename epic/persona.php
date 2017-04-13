8b870b4f3da1c33af23b6aa13e85333545fb3d06
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-04-13 10:31:23 -0600

    Wrote a persona as myself single POV for Photo Lover site; example

--- epic/persona.php
+++ epic/persona.php
@@ -9,10 +9,19 @@
 	<body>
 		<main>
 			<h1>
-				Photo Canvas Persona
+				 Persona Photo Lover
 			</h1>
-			<p> I'm a photographer and I want to share my photos to promote myself as an entrepeneur.
+			<p> I am a photographer and I want to find and share photos that inspire me. I love browsing through other artist's work when I come across one that astonishes me, I hit favorite and save it to my profile for later. Usually I am on my laptop at home in the evening when I browse, I can get lost for hours on Photo Lover searching, sharing and saving. Sometimes when I need a break from work or studying at a cafe I will log on and browse for a short while to escape.
 			</p>
+			<p>
+				Another wonderful service of Photo lover is the print service they provide. You can buy a printed version of your photos or another artist's. That's something I never learned how to do, making large scale prints the equipment alone is very expensive and you need skills I don't have to print them correctly.
+			</p>
+			<h2>To Summarize:</h2>
+			<ul>
+				<li>As a Photo lover I want to favorite photos</li>
+				<li>As a Photo lover I want to promote myself as a photgrapher</li>
+				<li> As a user I was to buy prints of photos</li>
+			</ul>
 		</main>
 	</body>
 </html>
