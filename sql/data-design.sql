90ea6ebab79a9da126de5daeb0368cbc6c1d3da9
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-04-13 22:34:34 -0600

    created mysql page base on profile concept model

---
+++ sql/data-design.sql
@@ -0,0 +1,17 @@
+mysql>
+DROP TABLE IF EXISTS 'favorite';
+
+CREATE TABLE profile (
+profileId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+profileUserName VARCHAR(128) UNIQUE NOT NULL,
+profileEmail VARCHAR(128) UNIQUE NOT NULL,
+profileActivationToken CHAR(32),
+profileAtHandle VARCHAR(32) NOT NULL,
+profileHash CHAR (128) NOT NULL,
+profileSalt CHAR (64) NOT NULL,
+UNIQUE(profileEmail),
+UNIQUE(profileAtHandle),
+
+PRIMARY KEY(profileId)
+
+);
