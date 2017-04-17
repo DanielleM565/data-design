1d79c240f0108b18350937b3f255788764bb9d9e
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-04-16 22:35:05 -0600

    tried to figure out some of the entities

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -20,11 +20,13 @@ PRIMARY KEY(profileId)
 CREATE TABLE product (
 
 	productId INT UNSIGNED AUTO_INCREMENT NOT NULL,
-	productContent VARCHAR(140) NOT NULL,
-	productDescription VARCHAR (320) NOT NULL,
+	/*productContent I plan for this to be a picture*/
+	productDescription VARCHAR (520) NOT NULL,
+	/* similar to tweet content in example */
 	productTitle VARCHAR(128) NOT NULL,
-	/*productPrice, */
-	/* not sure how to do a price */
+	/*productPrice, not sure how to do a price */
+	INDEX(productId),
+	PRIMARY KEY(productId),
 
 );
 
