9e48b83e3c77688f04ae073c66ef032be32672e0
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-04-26 13:01:39 -0600

    started product/index.php profile

---
+++ public_html/api/product/index.php
@@ -0,0 +1,59 @@
+<?php
+namespace Edu\Cnm\DataDesign;
+
+require_once("autoload.php");
+/**
+ * Small Cross Section of a bad etsy site product favorite message
+ *
+ * @author Danielle Martin
+ * @version 1
+ *
+ **/
+class product {
+/**
+ * id for this Tweet; this is the primary key
+ * @var int $tweetId
+ **/
+private $productId;
+/**
+ *id for the Profile that sent this Tweet; this is a foreign key
+ * @var int $productProfileId
+ **/
+private $productProfileId;
+/**
+ * actual textual description of what the product is
+ * @var string $productDescription
+ **/
+private $productDescription;
+	/**
+	 *Name/title of product
+	 * @var string $productTitle
+	 **/
+private $productTitle;
+
+/**
+ * constructor for this product
+ *
+ * @param int|null $newProductId id of this product or null if a new product
+ * @param int $newProductProfileId id of the profile that posted the product (seller)
+ *
+ **/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+?>
