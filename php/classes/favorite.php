26190c751e582350f959487a8ecafc71ac562b29
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-05-08 12:53:14 -0600

    moved product.php to the correct directory (had it in api's oops) and started the favorite.php

---
+++ php/classes/favorite.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Edu\Cnm\DataDesign
+require_once("autoload.php");
+
+/**
+ * Cross section of product like
+ *
+ * This is a cross section of what probably occurs when a profile favorites a product on etsy
+ * (weak entity) from m-to-n relationship between profile and product)
+ *
+ * @author Danielle Martin
+ * @version 1.0
+ **/
