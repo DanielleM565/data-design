09ecf616981fb551380b8e71df20ef5d2d5fb9ed
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-05-08 14:14:46 -0600

    started favorite.php class got accessor and mutator methods for favorite and started PDO stuff stopped just before delete

--- php/classes/favorite.php
+++ php/classes/favorite.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Edu\Cnm\DataDesign
+namespace Edu\Cnm\DataDesign;
 require_once("autoload.php");
 
 /**
@@ -12,3 +12,150 @@ require_once("autoload.php");
  * @author Danielle Martin
  * @version 1.0
  **/
+
+
+class Favorite implements \JsonSerializable {
+
+	/**
+	 * id of the Product being favourite-ed this is a component of a composite primary key (and a foreign key)
+	 * @var int $favoriteProductId
+	 **/
+	private $favoriteProductId;
+
+	/**
+	 * id of the Profile that favorite-ed this is also a component of a primary ket and a foreign key
+	 * @var int $favoriteProfileId
+	 **/
+	private $favoriteProfileId;
+
+
+	/**
+	 * constructor for this favorite
+	 *
+	 * @param int $newFavoriteProfileId id of the parent profile
+	 * @param int $newFavoriteProductId id of the parent favorite
+	 * @throws \Exception if some other exception occurs
+	 * @throws \TypeError if data types violate type hints
+	 * @Documentation https://php.net/manual/en/language.oop5.decon.php
+	 *
+	 **/
+
+	public function __construct(int $newFavoriteProfileId, int $newFavoriteProductId = null) {
+		try {
+			$this->setFavoriteProfileId($newFavoriteProfileId);
+			$this->setFavoriteProductId($newFavoriteProfileId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+
+			//determine what exception type was thrown
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+	}
+
+	/**
+	 * accessor method for profile id
+	 *
+	 * @return int value of profile id
+	 **/
+
+	public function getFavoriteProfileId() : int {
+		return ($this->favoriteProfileId);
+	}
+
+	/**
+	 * mutator method
+	 *
+	 * @param int $newProfileId new value of profile Id
+	 * @throws \RangeException if $newProfileId is not positive
+	 * @throws \TypeError if $newProfileId is not an integer
+	 **/
+
+	/**
+	 * @return mixed
+	 */
+	public function setFavoriteProfileId(int $newProfileId) : void {
+		//verify the profile id is positive
+		if($newProfileId <= 0) {
+				throw(new \RangeException("profile id is not possible, like do you even exist?"));
+		}
+		// convert and store the profile id
+		$this->favoriteProfileId = $newProfileId;
+
+	}
+
+	/**
+	 * accessor method for product id
+	 *
+	 * @return int value of product id
+	 **/
+
+	public function getFavoriteProductId() : int {
+		return ($this->favoriteProductId);
+	}
+	/**
+	 *mutator method for product Id
+	 *
+	 *@param int $newFavoriteProductId new value of product id
+	 *@throws \RangeException
+	 *@throws \TypeError if $newFavoriteProductId is not an integer
+	 **/
+
+	/**
+	 * @param int $favoriteProductId
+	 */
+	public function setFavoriteProductId(int $newFavoriteProductId) : void {
+		//verify the product id is positive
+		if($newFavoriteProductId <= 0) {
+			throw(new \RangeException("product id is not positive"));
+		}
+		// convert and store the profile id
+		$this->favoriteProductId = $newFavoriteProductId;
+	}
+
+	/**
+	 * insert statci function that puts info into mysql
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+
+	public function insert(\PDO $pdo) : void {
+		//ensure the object exists before inserting no empty data in sql!
+		if($this->favoriteProfileId === null || $this->favoriteProductId === null) {
+			throw(new \PDOException("not a valid favorite"));
+		}
+		//create query template
+		$query = "INSERT INTO favorite(favoriteProfileId, favoriteProductId) VALUES(:favoriteProfileId, :favoriteProductId)";
+		$statement = $pdo->prepare($query);
+
+		//bind the member variables to the place holders in the template
+		$parameters = ["favoriteProfileId" => $this->favoriteProfileId, "favoriteProductId" => $this->favoriteProductId];
+			$statement->execute($parameters);
+	}
+
+	/**
+	 * stopped at like 173 oub function delete
+	 **/
+
+
+
+
+
+
+
+
+/**
+ * formats the state variables for JSON serialization
+ *
+ * @return array resulting state variables to serialize
+ **/
+
+public function jsonSerialize() {
+	$fields = get_object_vars($this);
+	//format the date so that the front end can consume it
+	return($fields);
+}
+
+}
+?>
